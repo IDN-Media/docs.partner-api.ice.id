752cdb012cf600240d4721706e00a6a245e73302
Louis Larry <louis.larry@gmail.com>
2022-11-25 07:13:42 +0700

    feat: add styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import { remark } from "remark";
 import remarkGfm from "remark-gfm";
 import remarkGithub from "remark-github";
-// import "../styles/globals.css";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
