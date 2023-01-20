8bc433080fe7f0211f136e76b371311aca618d73
darkbcx <darkbcx@yahoo.com>
2023-01-20 12:26:48 +0700

    feat : add chat module doc

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,11 @@ export default function Home() {
             <Link href="swagger">Swagger</Link>
           </li>
           <li>
+            <Link href="chat-module">
+              Chat Module
+            </Link>
+          </li>
+          <li>
             <Link href="ice-notification-service">
               Ice Notification Service
             </Link>
